dfbdf81df900861103553c51ddf36ca9fb8b0632
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-09-25 19:17:02 -0500

    Added code for local storage of schedule entries

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,16 @@ var hourLabel = 9;
 var hourStr;
 var textAreaClass;
 var today = moment();
-var currentTime = moment().format("hh");
+var currentTime = moment().format("HH");
 
 // Array of schedule rows
- var scheduleBlocks = [];
+var scheduleBlocks = [];
 
 // Array of hour labels for each schedule row
 var hourBlocks = [];
+
+// Array of forms for each schedule row
+var formBlocks = [];
 
 // Array of event texts for each schedule row
 var textAreaBlocks = [];
@@ -21,44 +24,96 @@ var textAreaBlocks = [];
 // Array of text events for each schedule row
 var textAreaBlockStrs = [];
 
+// Array of save buttons
 var saveButtonBlocks = [];
 
-// Adds date to header
-headerEl.append(dateEl);
-dateEl.text(today.format("MMMM D YYYY"));
+function handleFormSubmit(event) {
+    event.preventDefault();
 
-for (var i = 0; i < 9; i++) {
-    hourLabel = (9 + i);
+    var newText = $(this).find('textarea').val();
+    var newTextName = $(this).find('textarea').attr("name");
+    console.log(newText);
+    console.log();
 
-    if (currentTime > hourLabel) {
-        textAreaClass = "past";
+    if (newTextName === "event-input 0") {
+        localStorage.setItem("event0", newText);
     }
-    else if (currentTime == hourLabel) {
-        textAreaClass = "present";
+    else if (newTextName === "event-input 1") {
+        localStorage.setItem("event1", newText);
     }
-    else {
-        textAreaClass = "future";
+    else if (newTextName === "event-input 2") {
+        localStorage.setItem("event2", newText);
     }
-
-    if (hourLabel > 12) {
-        hourLabel -= 12;
-        hourStr = hourLabel + "PM";
+    else if (newTextName === "event-input 3") {
+        localStorage.setItem("event3", newText);
     }
-    else if (hourLabel > 11) {
-        hourStr = hourLabel + "PM";
-    } 
-    else {
-        hourStr = hourLabel + "AM";
+    else if (newTextName === "event-input 4") {
+        localStorage.setItem("event4", newText);
     }
-
-    scheduleBlocks.push($('<div>').addClass('col-12 d-inline-flex').css('margin-bottom', '1px'));
-    hourBlocks.push($('<div>').addClass('hour col-1').text(hourStr)); 
-    textAreaBlocks.push($('<textarea>').addClass(`col-11 ${textAreaClass}`));
-    saveButtonBlocks.push($('<button>').addClass('saveBtn').append($('<i>').text('test')));
-    scheduleBlocks[i].append(hourBlocks[i]);
-    scheduleBlocks[i].append(textAreaBlocks[i]);
-    scheduleBlocks[i].append(saveButtonBlocks[i]);
-    rowEl.append(scheduleBlocks[i]);
+    else if (newTextName === "event-input 5") {
+        localStorage.setItem("event5", newText);
+    }
+    else if (newTextName === "event-input 6") {
+        localStorage.setItem("event6", newText);
+    }
+    else if (newTextName === "event-input 7") {
+        localStorage.setItem("event7", newText);
+    }
+    else if (newTextName === "event-input 8") {
+        localStorage.setItem("event8", newText);
+    }
 }
 
-containerEl.append(rowEl);
+// Adds date to header
+function init() {
+    headerEl.append(dateEl);
+    dateEl.text(today.format("MMMM D YYYY"));
+
+    for (var i = 0; i < 9; i++) {
+        hourLabel = (9 + i);
+    
+        if (currentTime > hourLabel) {
+            textAreaClass = "past";
+        }
+        else if (currentTime == hourLabel) {
+            textAreaClass = "present";
+        }
+        else {
+            textAreaClass = "future";
+        }
+
+        if (hourLabel > 12) {
+            hourLabel -= 12;
+            hourStr = hourLabel + "PM";
+        }
+        else if (hourLabel > 11) {
+            hourStr = hourLabel + "PM";
+        }
+        else {
+            hourStr = hourLabel + "AM";
+        }
+
+        scheduleBlocks.push($('<div>').addClass('col-12 d-inline-flex').css('margin-bottom', '1px'));
+        hourBlocks.push($('<div>').addClass('hour col-1').text(hourStr));
+        
+        textAreaBlocks.push($('<textarea>').addClass(`col-10 ${textAreaClass}`).attr('name', `event-input ${i}`));
+        saveButtonBlocks.push($('<button>').addClass('saveBtn col-1').attr('height', '80px').append($('<i>').text('Save')));
+        formBlocks.push($('<form>').addClass('col-12 d-inline-flex').append(textAreaBlocks[i]).append(saveButtonBlocks[i])
+            .on('submit', handleFormSubmit));
+        scheduleBlocks[i].append(hourBlocks[i]);
+        scheduleBlocks[i].append(formBlocks[i]);
+        rowEl.append(scheduleBlocks[i]);
+    }
+
+    containerEl.append(rowEl);
+}
+
+init();
+
+// for (var i = 0; i < saveButtonBlocks.length; i++) {
+//     saveButtonBlocks[i].addEventListener("click", function () {
+//         console.log("Button clicked");
+//     });
+// }
+
+// rowEl.on('submit', handleFormSubmit);
